a686cffaa24ccc01f23dd638d103257e9a011f4d
Lucero07 <ing.lucero.ramos@gmail.com>
2017-04-18 00:34:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+  var div1 = document.createElement("div");
+  var div2 = document.createElement("div");
+  var div3 = document.createElement("div");
+  var div4 = document.createElement("div");
+  var img1 = document.createElement("img");
+  var img2 = document.createElement("img");
+  var img3 = document.createElement("img");
+  var img4 = document.createElement("img");
+  var borrar1 = document.createElement("input");
+  var borrar2 = document.createElement("input");
+  var borrar3 = document.createElement("input");
+  var borrar4 = document.createElement("input");
+
+  var contenedor = document.getElementById('contenedor');
+
+  img1.src = "assets/panda-1.jpg";
+  img2.src = "assets/panda5.jpg";
+  img3.src = "assets/panda-3.jpg";
+  img4.src = "assets/panda-4.jpg";
+
+  borrar1.type = "button";
+  borrar1.value = "X";
+  borrar1.className = "botonBorrar";
+  borrar2.type = "button";
+  borrar2.value = "X";
+  borrar2.className = "botonBorrar";
+  borrar3.type = "button";
+  borrar3.value = "X";
+  borrar3.className = "botonBorrar";
+  borrar4.type = "button";
+  borrar4.value = "X";
+  borrar4.className = "botonBorrar";
+
+  div1.appendChild(img1);
+  div1.appendChild(borrar1);
+  div2.appendChild(img2);
+  div2.appendChild(borrar2);
+  div3.appendChild(img3);
+  div3.appendChild(borrar3);
+  div4.appendChild(img4);
+  div4.appendChild(borrar4);
+
+  contenedor.appendChild(div1);
+  contenedor.appendChild(div2);
+  contenedor.appendChild(div3);
+  contenedor.appendChild(div4);
